f5fea8c9f224d32116aa76910566e83c40dc0ba2
Lukáš Čech <ja@lukascech.cz>
2019-07-03 11:43:22 +0200

    Timezone-releated fixes

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const Harvest = require('harvest').default;
 const { IncomingWebhook } = require('@slack/webhook');
-const moment = require('moment');
+const moment = require('moment-timezone');
 const config = require('./config.json');
 
 const harvest = new Harvest({
@@ -50,6 +50,7 @@ const getStatus = (hours, billable) => {
 };
 
 const getTimeEntries = (user, startDate, endDate) => {
+
   return harvest.timeEntries
   .list({ user_id: user.id, from: startDate.toDate(), to: endDate.toDate() })
   .then((response) => {
@@ -58,16 +59,17 @@ const getTimeEntries = (user, startDate, endDate) => {
     let total = 0;
     let billable = 0;
     time_entries.forEach((time_entry) => {
-      total += Math.ceil(time_entry.hours * 2) / 2;
+      //console.log("Time Entry: " + JSON.stringify(time_entry));
+      total += Math.ceil(time_entry.hours * 4) / 4;
       if (time_entry.billable) {
-        billable += Math.ceil(time_entry.hours * 2) / 2;
+        billable += Math.ceil(time_entry.hours * 4) / 4;
       }
     });
 
     let billableStatus = getStatus(billable, true);
     let totalStatus = getStatus(total, false);
-    userStats += ":" + (billableStatus.color !== "" ? billableStatus.color + "_" : "") + "heart: " + ("  " + billable.toFixed(1)).slice(-4);
-    userStats += " / " + ("00" + total.toFixed(1)).slice(-4);
+    userStats += ":" + (billableStatus.color !== "" ? billableStatus.color + "_" : "") + "heart: " + ("\u2000\u2000" + billable.toFixed(1)).slice(-4);
+    userStats += " / " + ("\u2000\u2000" + total.toFixed(1)).slice(-4);
     //userStats += " / :" + (totalStatus.color !== "" ? totalStatus.color + "_" : "") + "heart: " + ("00" + total.toFixed(1)).slice(-4);
     userStats += " - *" + user.first_name + " " + user.last_name + "*";
     //userStats += " (vykázáno " + totalStatus.message + ", v placenejch " + billableStatus.message + ")";
@@ -82,18 +84,25 @@ const getTimeEntries = (user, startDate, endDate) => {
 
 (async () => {
   try {
+    const company = await harvest.company.get().then(company => {
+      console.log(company);
+    });
+
     const users = await getUsersWithRole(process.argv[2] || "WATA");
 
-    const startDate = moment().startOf('week').isoWeekday(1);
+    const startDate = moment().tz('Europe/Prague').startOf('week').isoWeekday(1);
     const endDate = startDate.clone().add(7, 'days');
 
+    console.log("startDate: " + startDate.toDate());
+    console.log("endDate: " + endDate.toDate());
+  
     const userStats = await Promise.all(users.map(user => getTimeEntries(user, startDate, endDate)));
 
-    //const url = config.SLACK_WEBHOOK_URL;      // send to #wata
-    const url = config.SLACK_WEBHOOK_URL_TEST; // send to #test_lce
+    const url = config.SLACK_WEBHOOK_URL;      // send to #wata
+    //const url = config.SLACK_WEBHOOK_URL_TEST; // send to #test_lce
     const webhook = new IncomingWebhook(url);
     // Send the notification
-    const message = "Statistiky za období " + startDate.format("D.M.YYYY") + " - " + endDate.format("D.M.YYYY") + "\n" + userStats.join("\n");
+    const message = "Statistiky za období " + startDate.format("D.M.YYYY") + " - " + endDate.subtract(1, 'days').format("D.M.YYYY") + "\n" + userStats.join("\n");
     console.log("Sending message:\n" + message);
 
     await webhook.send({
